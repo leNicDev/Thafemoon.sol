17c5611b77f3af34319308f085aaae7a7fe2bdc7
leNicDev <germanelectronix@gmail.com>
2021-05-13 22:50:42 +0200

    Fix IUniswapV2Router02 address

--- Thafemoon.sol
+++ Thafemoon.sol
@@ -748,7 +748,7 @@ contract ThafeMoon is Context, IERC20, Ownable {
     constructor () public {
         _rOwned[_msgSender()] = _rTotal;
         
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
